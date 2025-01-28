ae5ea18a87a65c6b278397827216b0db13db9fa8
전상은 <wowddok99@gmail.com>
2025-01-28 23:33:09 +0900

    feat: 테일윈드 적용 및 app.jsx에 동적 임포트 적용

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,26 +1,49 @@
 // import '../styles/global.css'
-import { globalStyles } from '../src/components/commons/styles/globalStyles';
+import { globalStyles } from '@/src/components/commons/styles/globalStyles';
 import { Global } from '@emotion/react';
 import LoadingProvider from '../src/components/commons/loading';
 import LayOut from '../src/components/commons/layout';
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter();
 
-  return (
+    // 레이아웃을 적용하지 않을 경로 설정
+    const excludedLayoutPaths = ['/login', '/signUp'];
+
+    // 현재 경로가 레이아웃을 적용하지 않을 경로인지 확인
+    const isExcludedLayout = excludedLayoutPaths.includes(router.pathname);
+
+    // excludedLayoutPaths에 해당하는 경로에서만 global.css를 동적 임포트
+    useEffect(() => {
+        if (isExcludedLayout) {
+            const loadStyles = async () => {
+                const styles = await import('../styles/global.css');
+            };
+            loadStyles();
+        }
+    }, [isExcludedLayout]);
+
+    return (
       <QueryClientProvider client={queryClient}>
         <LoadingProvider>
+        <Global styles={globalStyles}/>
           <div>
-            <Global styles={globalStyles}/>
-            <LayOut>
-              <Component {...pageProps} />
-            </LayOut>
+              {!isExcludedLayout ? (
+                  <LayOut>
+                      <Component {...pageProps} />
+                  </LayOut>
+              ) : (
+                  <Component {...pageProps} />
+              )}
           </div>
         </LoadingProvider>
       </QueryClientProvider>
-  )
+)
 
 }
 
